8c52f553b8cbee3e5000f8969b467338b0e24050
Andrey <pythonhunter146@gmail.com>
2023-09-29 22:13:39 +0500

    Basic IRC listener client

--- src/lib.rs
+++ src/lib.rs
@@ -1,54 +1,100 @@
+use std::thread::{spawn, JoinHandle};
+use futures::executor::block_on;
+use tokio::runtime::Builder;
+use irc::client::prelude::*;
+use futures::StreamExt;
 use std::sync::RwLock;
-use std::thread::{sleep, spawn, JoinHandle};
-use std::time::Duration;
+use std::ptr::NonNull;
+
+
+// Interoperability with the main program
 
 #[repr(C)] pub struct Color(u8, u8, u8);
-#[repr(C)] pub struct Message {
-    text: *mut String,
+#[repr(C)] pub struct Comment {
+    text: String,
     color: Color,
 }
 
 
-static NOTIFIER_THREAD: RwLock<Option<JoinHandle<()>>> = RwLock::new(None);
+// Multi-threading helpers
 
-fn message_generator_thread(send: unsafe extern "C" fn(Message) -> ()) -> () {
-    let ver = _version();
-    unsafe {send(Message {
-        text: Box::into_raw(Box::new(  format!("Notifier v{ver} started")  )),
-        color: Color(40, 40, 255)
-    });}
-    
-    for i in 0..65536 {
-        unsafe {send(Message {
-            text: Box::into_raw(Box::new(  format!("#{i}")  )),
-            color: Color(40, 255, 40)
-        });}
-        sleep(Duration::from_millis(1));
+type Thread = (JoinHandle<()>, Vec<Comment>, bool);
+static NOTIFIER_THREAD: RwLock<Option<Thread>> = RwLock::new(None);
+macro_rules! thread_stats {
+    (const) => {NOTIFIER_THREAD.read().unwrap().as_ref().unwrap()};
+    (mut)   => {NOTIFIER_THREAD.write().unwrap().as_mut().unwrap()};
+}
+
+#[inline] fn put_comment(comment: Comment) -> () {
+    thread_stats!(mut).1.push(comment);
+}
+
+
+// Async function listening to IRC messages
+
+async fn irc_listener(mut client: Client) -> () {
+    let mut stream = client.stream().expect("Stream was already taken");
+
+    while let Some(message) = stream.next().await.transpose().expect("message failure") {
+        put_comment(Comment { color: Color(255, 192, 127), text: message.to_string() });
+        if !thread_stats!(const).2 {break;}
     }
 }
 
-fn _version() -> u64 {2}
+fn message_generator_thread() -> () {
+    let ver = _version();
+    put_comment(Comment { color: Color(127, 127, 255),
+        text: format!("Notifier v{ver} started")});
+    
+    let futures_runtime = Builder::new_current_thread()
+        .enable_all()
+        .build().expect("cannot start async runtime");
+    
+    let irc_config = Config {
+        nickname: Some("ProgramCrafter".to_owned()),
+        server: Some("irc.esper.net".to_owned()),
+        channels: vec!["#cc.ru".to_owned()],
+        ..Config::default()
+    };
+    let client = futures_runtime.block_on(async {
+        Client::from_config(irc_config).await
+    }).expect("Connection failed");
+    client.identify().expect("Connection failed");
+    
+    put_comment(Comment { color: Color(127, 127, 255),
+        text: "Connected to #cc.ru as ProgramCrafter".to_owned()});
+    
+    futures_runtime.block_on(async {
+        irc_listener(client).await
+    });
+}
+
+fn _version() -> u64 {3}
 
 #[no_mangle] pub unsafe extern "C" fn version() -> u64 {_version()}
 
-#[no_mangle] pub unsafe extern "C" fn start(callback: unsafe extern "C" fn(Message) -> ()) -> bool {
+#[no_mangle] pub unsafe extern "C" fn start() -> () {
     let mut notifier = NOTIFIER_THREAD.write().unwrap();
     if notifier.is_none() {
-        *notifier = Some(spawn(move || message_generator_thread(callback)));
-        true
-    } else {
-        false
+        *notifier = Some((spawn(message_generator_thread), vec![], true));
     }
 }
 
 #[no_mangle] pub unsafe extern "C" fn stop() -> () {
     let mut notifier = NOTIFIER_THREAD.write().unwrap();
-    match notifier.take() {
-        Some(thread) => {thread.join().expect("Could not stop notifier thread");},
-        None         => {}
-    }
+    if notifier.is_none() {return;}
+    
+    notifier.as_mut().unwrap().2 = false;    // signal to stop working
+    drop(notifier);
+    
+    let mut notifier = NOTIFIER_THREAD.write().unwrap();
+    let (thread, _, _) = notifier.take().unwrap();
+    thread.join().expect("Could not stop notifier thread");
 }
 
-#[no_mangle] pub unsafe extern "C" fn deallocate_message(msg: Message) -> () {
-    let _ = Box::from_raw(msg.text);
+#[no_mangle] pub unsafe extern "C" fn sync_process_queue(mut messages: NonNull<Vec<Comment>>) -> () {
+    let mut notifier = NOTIFIER_THREAD.write().unwrap();
+    let our_queue: &mut Vec<Comment> = &mut notifier.as_mut().unwrap().1;
+    let app_queue = messages.as_mut();
+    app_queue.append(our_queue);
 }
